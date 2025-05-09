ae0b586854e403a54b8350eb2caf789bfffa0073
shohei-inoue <a20.mskr@g.chuo-u.ac.jp>
2025-05-09 18:09:49 +0900

    menu-item ui作成

---
+++ app/(pages)/(menu)/[id]/_components/MenuItemNav/MenuItemBottomNav.tsx
@@ -0,0 +1,14 @@
+const MenuItemBottomNav = () => {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-gray-100 flex justify-between items-center p-4 shadow">
+      <button className="bg-gray-500 py-2 px-4 rounded text-white">
+        カートに追加
+      </button>
+      <button className="bg-gray-900 py-2 px-4 rounded text-white">
+        注文確定
+      </button>
+    </nav>
+  )
+}
+
+export default MenuItemBottomNav
