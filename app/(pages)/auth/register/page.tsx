1c73cc81b6b7d199ecaeb89637801e4e41e951f1
shohei-inoue <a20.mskr@g.chuo-u.ac.jp>
2025-05-11 17:04:46 +0900

    館員登録画面

--- app/(pages)/auth/register/page.tsx
+++ app/(pages)/auth/register/page.tsx
@@ -1,22 +1,84 @@
+'use client'
+
+import Heading from "@/app/components/Heading/Heading";
+import MainContainer from "@/app/components/MainContainer/MainContainer";
+import MainContent from "@/app/components/MainContainer/MainContent";
+import { useRouter } from "next/navigation";
+
 export default function Register() {
+
+  const router = useRouter()
+
+  const toggleLogin = () => {
+    router.push("/auth/login")
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-3xl font-bold">Register</h1>
-      <form className="w-full max-w-sm mt-4">
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-          <input type="text" id="username" name="username" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500" />
+    <MainContainer>
+      <MainContent>
+        <Heading level={1}>会員登録</Heading>
+          {/* TODO デザイン調整必須 */}
+          <form className="flex flex-col gap-4 items-center w-full">
+            <div>
+              <label htmlFor="username">氏名</label>
+              <input 
+                type="text"
+                id="username"
+                name="username"
+                required 
+                className="color-gray-700 border border-gray-300 rounded-md p-2"
+                placeholder="山田太郎"
+              />
+            </div>
+            <div>
+              <label htmlFor="birthday">生年月日</label>
+              <input 
+                type="text"
+                id="birthday"
+                name="birthday"
+                required
+                className="color-gray-700 border border-gray-300 rounded-md p-2"
+                placeholder="YYYY/MM/DD"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">メールアドレス</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                className="color-gray-700 border border-gray-300 rounded-md p-2"
+                placeholder="example.com"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">パスワード</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                required
+                className="color-gray-700 border border-gray-300 rounded-md p-2"
+                placeholder="********"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-gray-500 text-white py-2 px-4 rounded">
+              登録
+            </button>
+          </form>
+          <div className="flex flex-col gap-4 items-center">
+            <p>すでにアカウントをお持ちですか？</p>
+            <button
+              type="button"
+              className="bg-gray-500 text-white py-2 px-4 rounded cursor-pointer"
+              onClick={toggleLogin}>
+              ログイン
+            </button>
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-          <input type="email" id="email" name="email" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500" />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-          <input type="password" id="password" name="password" required className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500" />
-        </div>
-        <button type="submit" className="w-full px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">Register</button>
-      </form>
-    </div>
+      </MainContent>
+    </MainContainer>
   )
 }
