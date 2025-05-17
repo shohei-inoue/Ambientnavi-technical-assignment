93b848b9ebffcd9d597aa9f8f0f0e69f19c85f18
shohei-inoue <a20.mskr@g.chuo-u.ac.jp>
2025-05-18 02:42:40 +0900

    Remove tracked uploaded images and apply .gitignore

--- app/(pages)/admin/(dashboard)/page.tsx
+++ app/(pages)/admin/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 
 import MainContainer from "@/app/components/MainContainer/MainContainer";
 import MainContent from "@/app/components/MainContainer/MainContent";
-import DashboardContents from "./_components/DashboardContents/DashboardContents2";
+import DashboardContents from "./_components/DashboardContents/DashBoardContents";
 
 export default function AdminDashBoard() {
   return (
