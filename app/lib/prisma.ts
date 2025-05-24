87cf2b360c14dda70d8d70e5e5da508351e882d6
shohei-inoue <a20.mskr@g.chuo-u.ac.jp>
2025-05-24 17:21:52 +0900

    lib/prismaにruntime追加

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { PrismaClient } from "../generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
@@ -7,7 +9,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ["query"],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
